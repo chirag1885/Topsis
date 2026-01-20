ffc4916e6095686518211976feb309760246a7e8
chirag1885 <chiragg1885@gmail.com>
2026-01-20 18:35:32 +0530

    webapp

---
+++ Web/client/src/App.jsx
@@ -0,0 +1,14 @@
+import TopsisForm from "./components/TopsisForm";
+
+export default function App() {
+  return (
+    <>
+      <div className="main-content">
+        <div className="container mt-5">
+          <h2 className="text-center mb-4">TOPSIS Web Service</h2>
+          <TopsisForm />
+        </div>
+      </div>
+    </>
+  );
+}
